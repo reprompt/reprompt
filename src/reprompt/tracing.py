68dca3abc1e422516e0efb2ba907705ba214954c
Rob <balian.rob@gmail.com>
2024-04-15 14:43:48 -0700

    RC: Removing monkeypatching for now, adding debug code, adding get_edits function

--- src/reprompt/tracing.py
+++ src/reprompt/tracing.py
@@ -5,14 +5,17 @@ from datetime import datetime
 import aiohttp
 import asyncio
 
+import logging
+
 from . import config
 
 from .background_task_manager import BackgroundTaskManager
 from functools import partial
 
+logger = logging.getLogger(__name__)
+
 
 async def write_traces_task(traces):
-    print("writing traces TASK")
     timestamp = datetime.now().isoformat()
     data = {"traces": [{"function_calls": traces, "timestamp": timestamp}]}
     if config.api_key is None:
@@ -20,17 +23,18 @@ async def write_traces_task(traces):
         return
     try:
         async with aiohttp.ClientSession() as session:
+            logger.debug(f"Uploading traces asynchronously")
             async with session.post(
                 f"{config.api_base_url}/api/tracer/upload_batch",
                 json=data,
                 headers={"Content-Type": "application/json", "apiKey": config.api_key},
             ) as response:
                 if response.status != 200:
-                    print("Failed to upload batch")
+                    logger.error(f"Failed to upload batch: {response.status}")
                 else:
-                    print("Batch uploaded successfully")
+                    logger.debug("Batch uploaded successfully")
     except aiohttp.ClientError:
-        print("Cannot connect to tracer")
+        logger.error("Cannot connect to reprompt to upload traces")
 
 
 def write_traces(traces):
@@ -45,6 +49,7 @@ def write_traces(traces):
 
 class FunctionTrace:
     def __init__(self, func_name, func_inputs):
+        logger.debug(f"Creating trace for function {func_name}")
         self.func_name = func_name
         self.start_time = datetime.now()
         self.end_time = None
@@ -53,6 +58,7 @@ class FunctionTrace:
         self.func_outputs = None
 
     def end_trace(self, func_outputs):
+        logger.debug(f"Ending trace for function {self.func_name}")
         self.func_outputs = func_outputs
         self.end_time = datetime.now()
         self.duration = (self.end_time - self.start_time).total_seconds()
@@ -66,3 +72,21 @@ class FunctionTrace:
             "function_inputs": self.func_inputs,
             "function_outputs": self.func_outputs,
         }
+
+
+async def get_edits(input: str) -> dict:
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.post(
+                f"{config.api_base_url}/api/overrides/get_example_overrides",
+                json={"input": input},
+                headers={"Content-Type": "application/json"},
+            ) as response:
+                if response.status != 200:
+                    logger.error(f"Failed to fetch edits: {response.status}")
+                    return None
+                else:
+                    logger.debug("fetched example overrides")
+                    return await response.json()
+    except aiohttp.ClientError:
+        logger.error("Cannot connect to reprompt to fetch edits")
